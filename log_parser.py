1be8601d49755514105254c1823c4b4e5a9d1f60
T@.com <136024334+tkhutswa@users.noreply.github.com>
2024-03-20 22:58:36 +0200

    Update log_parser.py

--- log_parser.py
+++ log_parser.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-log = {"timezone":"2024-02-07 15:45:44:799 EST","acode":"TGu_GcWUPi-TIdNenwyLrA","thread":"SmtpThread-12142339","level":"INFO ","mimemessage":"Connection|10.20.216.242|185.70.41.104"}
+log = {"timezone":"2024-02-07 15:45:44:799 EST","acode":"TGu_GcWUPi-TIdNenwyLrA","thread":"SmtpThread-12142339","level":"INFO ","mimemessage":"Connection|10.10.10.10|1.1.1.1"}
 
 #if you iterate using keys instead of values - we should get the info we looking for.
 for i in log.values():
