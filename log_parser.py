ac46e0a0541b87c838591dd65a421f9c8c9d2b1b
T@.com <136024334+tkhutswa@users.noreply.github.com>
2024-03-21 07:00:36 +0200

    Update log_parser.py

--- log_parser.py
+++ log_parser.py
@@ -6,3 +6,8 @@ log = {"timezone":"2024-02-07 15:45:44:799 EST","acode":"TGu_GcWUPi-TIdNenwyLrA"
 for i in log.values():
     print(i)
 #the mimemessage in this case is the key for connection | IP address :)
+
+#another way we can do it is to call both keys and value and iterate from there:
+
+for key, value in log.items():
+    print(key+ value)
