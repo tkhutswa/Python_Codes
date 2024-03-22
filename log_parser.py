56d9bde03247df41d10e73605fefb3f4d97e476b
T@.com <136024334+tkhutswa@users.noreply.github.com>
2024-03-22 14:29:34 +0200

    Update log_parser.py

--- log_parser.py
+++ log_parser.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-log = {"timezone":"2024-02-07 15:45:44:799 EST","acode":"TGu_GcWUPi-TIdNenwyLrA","thread":"SmtpThread-12142339","level":"INFO ","mimemessage":"Connection|10.20.216.242|185.70.41.104"}
+log = {"timezone":"2024-02-07 15:45:44:799 EST","acode":"TGu_Gci-TILrA","thread":"SmtpThread-1339","level":"INFO ","mimemessage":"Connection|3.120.6.2|18.700.410.104"}
 
 for key, value in log.items():
     print(f"{key} : {value}")
